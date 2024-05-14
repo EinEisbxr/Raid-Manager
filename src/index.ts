99105d56644a4aa2c3dfc69d810c7fdcf772b878
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-05-14 19:58:49 +0200

    chore: Add TCP server for handling connections on port 8000

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { startBot } from "./Discord_Bot/bot.js";
 import { PrismaClient } from "@prisma/client";
 import { clearOldDatabaseEntries } from "./Discord_Bot/functions/clearOldDatabaseEntries.js";
+import net from "net";
 
 export const prisma = new PrismaClient() as any;
 
@@ -9,3 +10,12 @@ export const client = await startBot();
 setInterval(async () => {
     await clearOldDatabaseEntries();
 }, 1000 * 60 * 60 * 24);
+
+// Create a TCP server
+const server = net.createServer((socket) => {
+    socket.write("Echo server\r\n");
+    socket.pipe(socket);
+});
+
+// Listen for a connection on port 8000
+server.listen(8000, "127.0.0.1");
