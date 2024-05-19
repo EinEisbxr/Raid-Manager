26e439a40447726e108e4639dd4b1c30689d1fdd
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-05-19 14:36:40 +0200

    chore: Add open_raid_post command to commands index

--- src/Discord_Bot/commands/index.ts
+++ src/Discord_Bot/commands/index.ts
@@ -1,11 +1,13 @@
-import * as setup_clan from "./setupClan.js";
-import * as get_raid_fails from "./getRaidFails.js";
-import * as get_current_raid_stats from "./getCurrentRaidStats.js";
-import * as raid_service_post from "./raidServicePost.js";
+import * as setup_clan from './setupClan.js';
+import * as get_raid_fails from './getRaidFails.js';
+import * as get_current_raid_stats from './getCurrentRaidStats.js';
+import * as raid_service_post from './raidServicePost.js';
+import * as open_raid_post from './openRaidPost.js';
 
 export const commands = {
     setup_clan,
     get_raid_fails,
     get_current_raid_stats,
     raid_service_post,
+    open_raid_post, 
 };
