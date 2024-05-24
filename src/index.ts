1351f4d268fa923ce246c797168b9a33add40c05
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-05-24 14:30:53 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { startBot } from "./Discord_Bot/bot.js";
-import { clearOldDatabaseEntries } from "./Discord_Bot/functions/clearOldDatabaseEntries.js";
-import net from "net";
-import pg from "pg";
+import { startBot } from './Discord_Bot/bot.js';
+import { clearOldDatabaseEntries } from './Discord_Bot/functions/clearOldDatabaseEntries.js';
+import net from 'net';
+import pg from 'pg';
 const { Pool } = pg;
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
+
+console.log(`Connecting to database at ${connectionString}`);
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
@@ -20,13 +22,13 @@ setInterval(async () => {
 
 // Create a TCP server
 const server = net.createServer((socket) => {
-    socket.write("Echo server\r\n");
+    socket.write('Echo server\r\n');
     socket.pipe(socket);
 
-    socket.on("error", (err) => {
+    socket.on('error', (err) => {
         console.error(`Socket error: ${err}`);
     });
 });
 
 // Listen for a connection on port 8000
-server.listen(8000, "0.0.0.0");
+server.listen(8000, '0.0.0.0');
