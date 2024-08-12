f2a63742b357230bb2d58d247c4373b6322d648b
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-08-12 10:34:11 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { startBot } from "./Discord_Bot/bot.js";
-import { clearOldDatabaseEntries } from "./Discord_Bot/functions/clearOldDatabaseEntries.js";
-import pg from "pg";
+import { startBot } from './Discord_Bot/bot.js';
+import { clearOldDatabaseEntries } from './Discord_Bot/functions/clearOldDatabaseEntries.js';
+import pg from 'pg';
 const { Pool } = pg;
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import net from 'net';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
@@ -14,6 +15,17 @@ const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
 
 export const client = await startBot();
+
+const server = net.createServer((socket) => {
+    socket.write('Echo server\r\n');
+    socket.pipe(socket as unknown as NodeJS.WritableStream);
+
+    socket.on('error', (err) => {
+        console.error(`Socket error: ${err}`);
+    });
+});
+
+server.listen(8000, '0.0.0.0');
 
 setInterval(async () => {
     await clearOldDatabaseEntries();
