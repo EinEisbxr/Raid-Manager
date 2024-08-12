1281d49c12f0aa75a65eed12307950c7b47fa2a1
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-08-12 12:19:48 +0200

    Update config.ts

--- src/Discord_Bot/config.ts
+++ src/Discord_Bot/config.ts
@@ -1,23 +1,23 @@
-import { Client } from "clashofclans.js";
-// import dotenv from "dotenv";
+import { Client } from 'clashofclans.js';
+// import dotenv from 'dotenv';
 
 // dotenv.config();
 
 const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
 
 export const ADMIN_ROLE_IDS: string[] = process.env.ADMIN_ROLE_IDS
-    ? process.env.ADMIN_ROLE_IDS.split(",").map((id) => id.trim())
+    ? process.env.ADMIN_ROLE_IDS.split(',').map((id) => id.trim())
     : [];
 
 if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
-    throw new Error("Missing environment variables");
+    throw new Error('Missing environment variables');
 }
 
-export const COC_API_TOKEN = process.env.COC_API_TOKEN ?? "";
+export const COC_API_TOKEN = process.env.COC_API_TOKEN ?? '';
 
 export const cocClient = new Client({
     keys: [COC_API_TOKEN],
-    baseURL: "https://cocproxy.royaleapi.dev/v1",
+    baseURL: 'https://cocproxy.royaleapi.dev/v1',
 });
 
 export const config = {
