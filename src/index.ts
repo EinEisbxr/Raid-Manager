7d9a3f8f89da03d9ef8911dd4a63b25e1ba450b5
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-11-12 21:34:10 +0100

    chore: Remove commented out deploy code

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,6 @@ import pg from 'pg';
 const { Pool } = pg;
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import http from 'http';
-
-// Set up an HTTP server for the Koyeb health check
-const healthCheckServer = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/health') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('OK');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    }
-});
-
-healthCheckServer.listen(8000, '0.0.0.0', () => {
-    console.log('Health check server is listening on port 8000');
-});
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
